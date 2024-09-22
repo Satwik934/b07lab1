a90b2a03ca29f1cf2613e28980a98088f4cd2431
Satwik Nethi <satwik.nethi@mail.utoronto.ca>
2024-09-22 18:01:54 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,45 @@
+public class Polynomial {
+    private double[] coeffi;
+
+    public Polynomial() {
+        this.coeffi = new double[]{0};	
+    }
+
+    public Polynomial(double[] coeff) {
+        coeffi = coeff;
+    }
+
+    public Polynomial add(Polynomial coeff2) {
+        int num1 = coeffi.length;
+        int num2 = coeff2.coeffi.length;
+        int greatestnum = Math.max(num1, num2);
+        
+        double[] resultantarr = new double[greatestnum];
+        
+        for (int i = 0; i < greatestnum; i++) {
+            if (i < Math.min(num1, num2)) {
+                // Add corresponding coefficients
+                resultantarr[i] = coeffi[i] + coeff2.coeffi[i];
+            } else if (i >= num2) {
+                // If coeff2 is shorter, take the remaining from this
+                resultantarr[i] = coeffi[i];
+            } else if (i >= num1) {
+                // If this is shorter, take the remaining from coeff2
+                resultantarr[i] = coeff2.coeffi[i];
+            }
+        }
+        return new Polynomial(resultantarr);
+    }
+
+    public double evaluate(double x1) {
+        double rel = 0;
+        for (int i = 0; i < coeffi.length; i++) {
+            rel = rel + (coeffi[i] * Math.pow(x1, i));
+        }
+        return rel;
+    }
+     
+    public boolean hasRoot(double x2) {
+        return evaluate(x2) == 0;
+    }
+}
