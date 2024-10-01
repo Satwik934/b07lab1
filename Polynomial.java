818c91a8e666f802f526253eddb67c2ccc9f8ca9
Satwiknethi <156567939+Satwik934@users.noreply.github.com>
2024-09-30 23:30:41 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 public class Polynomial {
     private double[] coeffi;
-    private int[] exponential;
+    private int[] exponential1;
 
     public Polynomial() {
         coeffi = new double[]{0};
-        exponential = new int[]{0};
+        exponential1 = new int[]{0};
     }
 
     public Polynomial(double[] coeff2, int[] expo2) {
         coeffi = coeff2;
-        exponential = expo2;
+        exponential1 = expo2;
     }
 
     private void ressizesarr(int size11) {
@@ -22,47 +22,64 @@ public class Polynomial {
         int[] sizedreexpo = new int[size11];
 
         System.arraycopy(coeffi, 0, sizedrecoeff, 0, size11);
-        System.arraycopy(exponential, 0, sizedreexpo, 0, size11);
+        System.arraycopy(exponential1, 0, sizedreexpo, 0, size11);
 
         coeffi = sizedrecoeff;
-        exponential = sizedreexpo;
+        exponential1 = sizedreexpo;
     }
 
-    private String readPoyfile(File file1) throws IOException {
+   private String readPoyfile(File file1) throws IOException {
         Scanner scanner1 = new Scanner(file1);
         String polnystr = scanner1.nextLine();
         scanner1.close();
         return polnystr;
     }
 
-    private void parspolystr(String polnystr) {
-        int maxnumes = polnystr.length() / 2 + 1;
-        coeffi = new double[maxnumes];
-        exponential = new int[maxnumes];
+   private void parspolystr(String polnystr) {
+    int maxnumes = (polnystr.length() / 2) + 1;
+    coeffi = new double[maxnumes];
+    exponential1 = new int[maxnumes];
 
-        String[] numterms = polnystr.split("(?=\\+)|(?=\\-)");
-        int numcount = 0;
+    String[] numterms = polnystr.split("(?=\\+)|(?=\\-)");
 
-        for (String term1 : numterms) {
-            term1 = term1.trim();
-            if (term1.contains("x")) {
-                String[] part1 = term1.split("x\\^");
-                double coeff23 = part1[0].isEmpty() || part1[0].equals("+") ? 1 :
-                                 part1[0].equals("-") ? -1 : Double.parseDouble(part1[0]);
-                int exp22er = part1.length > 1 ? Integer.parseInt(part1[1]) : 1;
+    int numcount = 0;
 
-                coeffi[numcount] = coeff23;
-                exponential[numcount] = exp22er;
-                numcount++;
+    for (String term12 : numterms) {
+        term12 = term12.trim();
+
+        if (term12.contains("x")) {
+            String[] part1 = term12.split("x");
+
+            double coeff23;
+            if (part1[0].isEmpty() || part1[0].equals("+")) {
+                coeff23 = 1;
+            } else if (part1[0].equals("-")) {
+                coeff23 = -1;
             } else {
-                coeffi[numcount] = Double.parseDouble(term1);
-                exponential[numcount] = 0;
-                numcount++;
+                coeff23 = Double.parseDouble(part1[0]);
             }
-        }
 
-        ressizesarr(numcount);
+            int exp22er;
+	    if (part1.length > 1) {
+    		exp22er = Integer.parseInt(part1[1]);
+	    } else {
+    		exp22er = 1;
+	    }
+
+            coeffi[numcount] = coeff23;
+            exponential1[numcount] = exp22er;
+            numcount++;
+            }
+	 else {
+            coeffi[numcount] = Double.parseDouble(term12);
+            exponential1[numcount] = 0;
+            numcount++;
+        }
     }
+
+    ressizesarr(numcount);
+    }
+
 
     public Polynomial(File file1) throws IOException {
         String polnystr = readPoyfile(file1);
@@ -84,41 +101,43 @@ public class Polynomial {
         double addition = 0;
 
         while (per < numm1 && qer < numm2) {
-            if (exponential[per] == coeff2.exponential[qer]) {
+            if (exponential1[per] == coeff2.exponential1[qer]) {
                 addition = coeffi[per] + coeff2.coeffi[qer];
                 if (addition != 0) {
                     resultantCoeff[rer] = addition;
-                    resultantExpo[rer] = exponential[per];
+                    resultantExpo[rer] = exponential1[per];
                     rer++;
                 }
                 per++;
                 qer++;
-            } else if (this.exponential[per] > coeff2.exponential[qer]) {
+            } else if (exponential1[per] > coeff2.exponential1[qer]) {
                 resultantCoeff[rer] = coeffi[per];
-                resultantExpo[rer] = exponential[per];
+                resultantExpo[rer] = exponential1[per];
                 rer++;
                 per++;
             } else {
                 resultantCoeff[rer] = coeff2.coeffi[qer];
-                resultantExpo[rer] = coeff2.exponential[qer];
+                resultantExpo[rer] = coeff2.exponential1[qer];
                 rer++;
                 qer++;
             }
         }
 
-        while (per < numm1) {
-            resultantCoeff[rer] = coeffi[per];
-            resultantExpo[rer] = exponential[per];
-            per++;
-            rer++;
-        }
-
-        while (qer < numm2) {
-            resultantCoeff[rer] = coeff2.coeffi[qer];
-            resultantExpo[rer] = coeff2.exponential[qer];
-            qer++;
-            rer++;
-        }
+        while (per < numm1 || qer < numm2) {
+    		if (per < numm1) {
+        		resultantCoeff[rer] = coeffi[per];
+        		resultantExpo[rer] = exponential1[per];
+        		per++;
+        		rer++;
+    		}
+    
+    		if (qer < numm2) {
+    	    		resultantCoeff[rer] = coeff2.coeffi[qer];
+        		resultantExpo[rer] = coeff2.exponential1[qer];
+        		qer++;
+        		rer++;
+    		}
+	}
 
         double[] overallcoeff = new double[rer];
         int[] overallexpo = new int[rer];
@@ -132,52 +151,50 @@ public class Polynomial {
     }
 
     private int addcombterm(double knewcoeff, int knewexp, double[] partcoeff, int[] partexpo, int ressize) {
-        for (int k2 = 0; k2 < ressize; k2++) {
-            if (partexpo[k2] == knewexp) {
-                partcoeff[k2] += knewcoeff;
-                return ressize;
+    	for (int k2 = 0; k2 < ressize; k2++) {
+    	    if (partexpo[k2] == knewexp) {
+            	partcoeff[k2] += knewcoeff;
+            	return ressize;
             }
         }
-        partcoeff[ressize] = knewcoeff;
-        partexpo[ressize] = knewexp;
-        return ressize + 1;
-    }
+    	partcoeff[ressize] = knewcoeff;
+    	partexpo[ressize] = knewexp;
+    	return ressize + 1;
+    }   
 
-    private int muleveryterm(double[] coeff1, int[] expo, Polynomial coeff3, double[] partcoeff, int[] partexpo, int ressize) {
-        for (int j = 0; j < coeff3.coeffi.length; j++) {
-            for (int i = 0; i < coeff1.length; i++) {
-                double knewcoeff = coeff1[i] * coeff3.coeffi[j];
-                int knewexp = expo[i] + coeff3.exponential[j];
-                ressize = addcombterm(knewcoeff, knewexp, partcoeff, partexpo, ressize);
-            }
-        }
-        return ressize;
-    }
+   
 
     public Polynomial multiply(Polynomial coeff3) {
-        int nummax2 = coeffi.length * coeff3.coeffi.length;
-        double[] partcoeff = new double[nummax2];
-        int[] partexpo = new int[nummax2];
-        int overallsize = 0;
+    	int nummax2 = coeffi.length * coeff3.coeffi.length;
+    	double[] partcoeff = new double[nummax2];
+    	int[] partexpo = new int[nummax2];
+    	int overallsize = 0;
 
-        for (int i = 0; i < coeffi.length; i++) {
-            overallsize = muleveryterm(coeffi, exponential, coeff3, partcoeff, partexpo, overallsize);
-        }
+    	for (int i = 0; i < coeffi.length; i++) {
+        	for (int j = 0; j < coeff3.coeffi.length; j++) {
+            		double knewcoeff = coeffi[i] * coeff3.coeffi[j];
+            		int knewexp = exponential1[i] + coeff3.exponential1[j];
+            		overallsize = addcombterm(knewcoeff, knewexp, partcoeff, partexpo, overallsize);
+        	}
+    	}
 
-        double[] overallcoefff = new double[overallsize];
-        int[] overallexpo2 = new int[overallsize];
-        for (int k1 = 0; k1 < overallsize; k1++) {
-            overallcoefff[k1] = partcoeff[k1];
-            overallexpo2[k1] = partexpo[k1];
-        }
+    	double[] overallcoefff = new double[overallsize];
+    	int[] overallexpo2 = new int[overallsize];
+    	for (int k1 = 0; k1 < overallsize; k1++) {
+        	overallcoefff[k1] = partcoeff[k1];
+        	overallexpo2[k1] = partexpo[k1];
+    	}
 
-        return new Polynomial(overallcoefff, overallexpo2);
+    	return new Polynomial(overallcoefff, overallexpo2);
     }
+
+
+  
 
     public double evaluate(double x1) {
         double result = 0;
         for (int i = 0; i < coeffi.length; i++) {
-            double num3 = Math.pow(x1, exponential[i]);
+            double num3 = Math.pow(x1, exponential1[i]);
             result += coeffi[i] * num3;
         }
         return result;
@@ -197,10 +214,10 @@ public class Polynomial {
                 polystr.append("+");
             }
 
-            if (exponential[ij] == 0) {
+            if (exponential1[ij] == 0) {
                 polystr.append(coeffi[ij]);
             } else {
-                polystr.append(coeffi[ij]).append("x^").append(exponential[ij]);
+                polystr.append(coeffi[ij]).append("x^").append(exponential1[ij]);
             }
             ij++;
         }
